cf4dacee79e0886661f70a39b9803e491582a637
charanvir <charanvir123@gmail.com>
2022-03-11 05:02:08 -0500

    timeBlocks change color according to current time, to account for past, present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let momentDate = moment().format("dddd, MMMM, Do, YYYY");
 let dateText = document.querySelector("#currentDay");
-let task = "";
-let paragraph = document.querySelector(".textarea");
+let timeBlock = document.querySelectorAll("textarea");
+let currenthour = moment().hour();
 
 // display the current date in the webpage
 function displayDate() {
@@ -14,38 +14,20 @@ setTimeout(function () {
     location.reload();
 }, (1000 * 60) * 60);
 
-// textarea is clicked
-$(".time-block").on("click", "p", function () {
-    let text = $(this)
-        .text()
-        .trim();
 
-    let newText = $("<textarea>").addClass("col-10").val(text);
-    $(this).replaceWith(newText);
+// check the current time of day and apply classes accordingly
+let timeOfDay = function () {
+    for (let i = 0; i < timeBlock.length; i++) {
+        if (timeBlock[i].id > currenthour) {
+            timeBlock[i].classList.add("future");
+        } else if (timeBlock[i].id < currenthour) {
+            timeBlock[i].classList.add("past");
+        } else {
+            timeBlock[i].classList.add("present");
+        };
+    };
+};
 
-    newText.trigger("focus");
-});
+timeOfDay();
 
-// text area is unclicked
-$(".time-block").on("blur", "textarea", function () {
-    let text = $(this).val();
 
-    let para = $("<p>").addClass("col-10 textarea").text(text);
-
-    $(this).replaceWith(para);
-
-    task += text;
-    saveTasks();
-});
-
-// save text to localStorage when save button is clicked
-let saveTasks = function () {
-    localStorage.setItem("task", task);
-}
-
-let loadTasks = function () {
-    let savedTask = localStorage.getItem("task");
-    paragraph.innerHTML = savedTask;
-}
-
-loadTasks();
