1ea1a9839d7dfe4ab48b3eeec7deb2f545f44d80
charanvir <charanvir123@gmail.com>
2022-03-12 02:12:54 -0500

    shortened code and retained functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let momentDate = moment().format("dddd, MMMM, Do, YYYY");
 let dateText = document.querySelector("#currentDay");
 let timeBlock = document.querySelectorAll("textarea");
 let currenthour = moment().hour();
-let saveButtons = document.querySelectorAll("[data-id]");
+var saveButtons = document.querySelectorAll(".saveBtn");
 
 // display the current date in the webpage
 function displayDate() {
@@ -28,5 +28,33 @@ let timeOfDay = function () {
         };
     };
 };
+
+
+// save text into localStorage when button is clicked
+$(".saveBtn").on("click", function () {
+    let time = $(this).parent().attr("id");
+    let text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+})
+
+// FIRST ATTEMPT AT GET ITEM
+// $("#9").val(localStorage.getItem("hour-9"))
+// $("#10").val(localStorage.getItem("hour-10"))
+// $("#11").val(localStorage.getItem("hour-11"))
+// $("#12").val(localStorage.getItem("hour-12"))
+// $("#13").val(localStorage.getItem("hour-13"))
+// $("#14").val(localStorage.getItem("hour-14"))
+// $("#15").val(localStorage.getItem("hour-15"))
+// $("#16").val(localStorage.getItem("hour-16"))
+// $("#17").val(localStorage.getItem("hour-17"))
+
+
+// WORKING ATTEMPT AT GET ITEM
+$(".description").each(function () {
+    let time = $(this).parent().attr("id");
+    let text = localStorage.getItem(time);
+    $(this).val(text);
+})
+
 timeOfDay();
 
